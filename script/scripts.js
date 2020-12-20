28af353353caf9e717b3f4723c15dd4a28a0c6ba
PlehanovEvgeniy <72038550+PlehanovEvgeniy@users.noreply.github.com>
2020-12-20 21:18:06 +0300

    Первый пуш

--- script/scripts.js
+++ script/scripts.js
@@ -1,14 +1,17 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-let popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
 
-let name = document.getElementById('name');
-let prof = document.getElementById('prof');
+const name = document.getElementById('name');
+const prof = document.getElementById('prof');
  
-let popupName = document.getElementById('popup-name');
-let popupProf = document.getElementById('popup-prof');
-let popupContainer = document.querySelector('.popup__container');
+const popupName = document.getElementById('popup-name');
+const popupProf = document.getElementById('popup-prof');
+const popupContainer = document.querySelector('.popup__container');
  
+const cardTemplate = document.querySelector('#card_template');
+const cardList = document.querySelector('.elements__container');
+
 function openPopup() {
     popupName.value = name.textContent;
     popupProf.value = prof.textContent;
@@ -30,3 +33,47 @@ function popupSubmitHandler(evt) {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 popupContainer.addEventListener('submit', popupSubmitHandler);
+
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function createFirstCards() {
+    const showCards = initialCards.map(showFirstCards);
+    cardList.append(...showCards);
+}
+createFirstCards();
+
+function showFirstCards ({ name, link }) {
+    const newCard = cardTemplate.content.cloneNode(true);
+    const cardName = newCard.querySelector('.element__text');
+    const cardLink = newCard.querySelector('.element__image');
+    cardName.textContent = name;
+    cardLink.src = link;
+    cardLink.alt = name;
+    return newCard
+}
