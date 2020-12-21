60d665b910f3fdacb158d8c6e88bf099939d2ec4
PlehanovEvgeniy <72038550+PlehanovEvgeniy@users.noreply.github.com>
2020-12-21 15:35:43 +0300

    Добавил Pointer , исправил Js

--- script/scripts.js
+++ script/scripts.js
@@ -1,5 +1,5 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelectorAll('.button_type_close');
+const closeButtons = document.querySelectorAll('.button_type_close');
 const addButton = document.querySelector('.profile__add-button-image');
 
 const popUpEdit = document.querySelector('.popup_type_edit');
@@ -59,31 +59,31 @@ function showFirstCards (item) {
     const cardName = newCard.querySelector('.element__text');
     cardName.textContent = item.name;
     const cardLink = newCard.querySelector('.element__image');
-    cardName.textContent = name;
     cardLink.src = item.link;
     cardLink.alt = item.name;
 
-    newCard.querySelector('.element__like').addEventListener('click', (evt) => {evt.target.classList.toggle('.button_clicked');});
+    newCard.querySelector('.element__like').addEventListener('click', (evt) => {evt.target.classList.toggle('button_clicked');});
 
     newCard.querySelector(".element__delete").addEventListener('click', (evt) => {
         evt.target.closest('.element').remove();
       });
 
-    cardLink.addEventListener('click', PreviewPicture);
+    cardLink.addEventListener('click', showPreviewPicture);
     return newCard
 }
 
-function PreviewPicture(evt) {
+function showPreviewPicture(evt) {
     const clickForOpen = evt.target.closest('.element');
     const openImageName = clickForOpen.querySelector('.element__text').textContent;
     const openImageLink = clickForOpen.querySelector('.element__image').src;
+    imageTitle.textContent = openImageName;
     imageLink.src = openImageLink;
     imageLink.alt = openImageName;
     openPopUp(popUpOpen);
 }
 
 function openPopUp(popup) {
-    popup.classList.add('.popup_opened')
+    popup.classList.add('popup_opened');
 }
 
 function closePopUp(popup) {
@@ -96,16 +96,16 @@ function editPopUp() {
     openPopUp(popUpEdit);
 }
 
-function CardFormSubmit(evt) {
+function showCardFormSubmit(evt) {
     evt.preventDefault();
     const newCardName = cardNameInput.value;
     const newCardLink = cardLinkInput.value;
-    const newCard = composeItem({ name: newCardName, link: newCardLink});
+    const newCard = showFirstCards({ name: newCardName, link: newCardLink});
     cardList.prepend(newCard);
     closePopUp(popUpAdd);
 }
 
-function ProfileFormSubmit(evt) {
+function showProfileFormSubmit(evt) {
     evt.preventDefault();
     nameOnProfile.textContent = nameInput.value;
     profOnProfile.textContent = profInput.value;
@@ -115,15 +115,15 @@ function ProfileFormSubmit(evt) {
 editButton.addEventListener('click', editPopUp);
 addButton.addEventListener('click', () => openPopUp(popUpAdd));
 
-closeButton.forEach(function(item){
+closeButtons.forEach(function(item){
     item.addEventListener('click', function closePopUp(evt) {
         const popUpToClose = evt.target.closest('.popup');
         popUpToClose.classList.remove('popup_opened');
     });
 });
 
-formElement.addEventListener('submit', ProfileFormSubmit);
-newCardElement.addEventListener('submit', CardFormSubmit);
+formElement.addEventListener('submit', showProfileFormSubmit);
+newCardElement.addEventListener('submit', showCardFormSubmit);
 
 
 createFirstCards();
